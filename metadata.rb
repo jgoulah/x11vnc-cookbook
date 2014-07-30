e661130ec978e637232428856e9dcc8dd2cef0df
miguelaferreira <miguelferreira@me.com>
2014-07-30 15:05:09 +0200

    Add support for centos (and others in the RH family)

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,6 @@ maintainer_email "jgoulah@gmail.com"
 license          "BSD"
 description      "Installs/Configures x11vnc"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
+
+supports         'centos'
