f7a828b50517942ff1d42df917b2d1451d43b168
miguelaferreira <miguelferreira@me.com>
2014-07-31 22:43:59 +0200

    Multiple changes: - Make service run with the same parameters in centos and debian - Render x11vnc.conf only for non centos systems - Add cookbook name to metadata
    Also fix some style issues repoted by
    foodcritic and rubocop. Replace " by ' when possible,
    use strings for indexing instead of symbols,
    and use new hash syntax instead of '=>'

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,14 @@
-default[:x11vnc] = {
-  :password => ""
+default['x11vnc'] = {
+  password: ''
 }
 
-default[:x11vnc][:display] = ':99'
-default[:x11vnc][:log_file] = '/var/log/x11vnc'
-default[:x11vnc][:executable_args] = '-nowf -nopw  -xkb'
-
+default['x11vnc']['display'] = ':99'
+default['x11vnc']['log_file'] = '/var/log/x11vnc'
+default['x11vnc']['executable_args'] = '-nowf \
+                                        -nopw \
+                                        -xkb  \
+                                        -xkb \
+                                        -noxrecord \
+                                        -noxfixes \
+                                        -noxdamage \
+                                        -forever'
