8b67e66a7eaa039031e297446b38748a007ffdae
John Goulah <jgoulah@gmail.com>
2014-08-06 22:28:56 -0400

    this seems to work better on :0 for me as a default

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['x11vnc'] = {
   :password => ''
 }
-default['x11vnc']['display'] = ':99'
+default['x11vnc']['display'] = ':0'
 default['x11vnc']['log_file'] = '/var/log/x11vnc'
 default['x11vnc']['executable_args'] = '-nowf -nopw -xkb -xkb '
 default['x11vnc']['executable_args'] << '-noxrecord -noxfixes '
