fcb6b019644f211c57062945ac5a8c01d80d1870
John Goulah <jgoulah@gmail.com>
2014-08-23 12:36:23 -0400

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jgoulah@gmail.com'
 license          'BSD'
 description      'Installs/Configures x11vnc'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.5'
+version          '0.0.6'
 
 %w{ ubuntu debian centos }.each do |os|
   supports os
